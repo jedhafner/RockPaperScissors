c4ff669a449c7b47bf1c292c32ae7b80792d1345
Jed Hafner <jedhafner@gmail.com>
2021-04-07 14:34:39 -0700

    added CSS styling, background image, and re-wrote script for interactivity in the browser.

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ let gameSummary;
 let playerSelection;
 let computerSelection;
 
-const scores = document.createElement('p');
-const resultDisplay = document.createElement('p');
-const robotTalk = document.createElement('p');
-const computerChooses = document.createElement('p');
+function chooseEncouragements(){
+    
+}
+
+const display = document.querySelector('.display');
+
+const weaponOfChoice = document.querySelector(".weaponOfChoice")
+const playerWeapon = document.querySelector("#playerWeapon");
+const compWeapon = document.querySelector("#compWeapon");
+const scoreDisplay = document.querySelector("#scoreDisplay");
+const resultsDisplay = document.querySelector("#resultsDisplay");
+
 
 
 /*Write a function called computerPlay that randomly returns/
@@ -22,13 +30,11 @@ function computerPlay(){
         case 2: return 'scissors';
     }
 }
+let throwWin = '';
 
 function playRound(playerSelection, computerSelection){
-/* add code that evaluates input and prompts again if selection is not rock, paper, or scissors.*/
-    let playerWins = `Player scores! ${playerSelection} beats ${computerSelection}.`;
-    let computerWins = `Computer scores. ${computerSelection} beats ${playerSelection}`;
     if (playerSelection === computerSelection){
-        return "It's a...tie? Go again!";
+        return;
     } else if (playerSelection === "rock"){
         if (computerSelection === "paper"){
             computerScore++;
@@ -53,9 +59,8 @@ function resetScore() {
     playerScore = 0;
     computerScore = 0;
 }
-const section = document.querySelector("section");
+const section = document.querySelector('section');
 section.addEventListener('click', Event => {
-    resultDisplay.textContent = "";
     playerSelection = Event.target.id;
     computerSelection = computerPlay();
     console.log(playerSelection);
@@ -63,28 +68,20 @@ section.addEventListener('click', Event => {
     playRound(playerSelection, computerSelection);
     console.log(playerScore);
     console.log(computerScore);
-    computerChooses.textContent = `I CHOOSE ${computerSelection}`
-    scores.textContent = `player score: ${playerScore} computer score: ${computerScore}`;
+    let scores = `player score: ${playerScore} computer score: ${computerScore}`;
+    scoreDisplay.textContent = scores;  
+    console.log(scores);
+    playerWeapon.textContent = playerSelection;
+    compWeapon.textContent = computerSelection;
     if (playerScore == 5){
-        resultDisplay.textContent = "You win!!!";
-        scores.textContent = "You made it to 5!";
+        resultsDisplay.textContent = "You win!!!";
         resetScore();
     } else if (computerScore == 5){
-        resultDisplay.textContent = "COMPUTER WINS.";
-        scores.textContent = "I HAVE 5. HA. HA.";
+        resultsDisplay.textContent = "You lose...Select your weapon to try again.";
         resetScore();
+    } else {
+        resultsDisplay.textContent = "Who will be victorious?";
     }
-
-
+    
 });
 
-
-
-const computer = document.querySelector('#computer');
-computer.appendChild(computerChooses);
-
-
-const display = document.querySelector('#Display');
-display.append(robotTalk);
-display.appendChild(scores);  
-display.appendChild(resultDisplay);
